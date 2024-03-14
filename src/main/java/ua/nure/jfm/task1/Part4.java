c4f6b30284b13de995e58eb5ab91b4075b274581
RuslanUC <dev_ruslan_uc@protonmail.com>
2024-03-14 16:11:20 +0200

    task 4 done

--- src/main/java/ua/nure/jfm/task1/Part4.java
+++ src/main/java/ua/nure/jfm/task1/Part4.java
@@ -3,11 +3,53 @@ package ua.nure.jfm.task1;
 public class Part4 {
 	
     public static void main(String[] args) {
-    	
+    	int num = parseInt(args[0]);
+
+        System.out.printf("%d = ", num);
+
+        int currentPrime = 2;
+        int currentExp = 0;
+        boolean firstPrime = true;
+        while(num > 1) {
+            while(num % currentPrime == 0) {
+                num /= currentPrime;
+                currentExp++;
+            }
+            if(currentExp > 0) {
+                if(!firstPrime)
+                    System.out.print(" x ");
+                System.out.printf("%d^%d", currentPrime, currentExp);
+                firstPrime = false;
+            }
+            currentPrime = nextPrime(currentPrime);
+            currentExp = 0;
+        }
     }
 
     public static int nextPrime(int x) {
-    	return 0;
+        int current = x+1;
+        while(!isPrime(current))
+            current++;
+
+        return current;
+    }
+
+    private static boolean isPrime(int n) {
+        for (int i = 2; i <= n / 2; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+    private static int parseInt(String str) {
+        int result = 0;
+        for(char ch : str.toCharArray()) {
+            result *= 10;
+            result += ch - '0';
+        }
+
+        return result;
     }
 }
 
